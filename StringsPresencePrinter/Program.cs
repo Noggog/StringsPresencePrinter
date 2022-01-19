2be68c70c432b7e36e2209fb30645dd998bab017
Justin Swanson <justin.c.swanson@gmail.com>
2022-01-19 00:16:25 -0600

    Initial commit

---
+++ StringsPresencePrinter/Program.cs
@@ -0,0 +1,20 @@
+﻿using Mutagen.Bethesda;
+using Mutagen.Bethesda.Environments;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Strings;
+using Noggog;
+
+Console.WriteLine("Hello, World!");
+
+var env = GameEnvironment.Typical.Skyrim(SkyrimRelease.SkyrimLE);
+
+var lookupOverlay = StringsFolderLookupOverlay.TypicalFactory(GameRelease.SkyrimLE, "Skyrim.esm", env.DataFolderPath, null);
+
+foreach (var src in EnumExt.GetValues<StringsSource>())
+{
+    Console.WriteLine($"{src}:");
+    foreach (var lang in lookupOverlay.AvailableLanguages(src))
+    {
+        Console.WriteLine($"   {lang}");
+    }
+}
